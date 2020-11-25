a315613bf0275425ff5b5bf689475de532c790e3
Soumya Shree A P <soumyashree.ap@robosoftin.com>
2020-11-26 00:41:36 +0530

    Create EmpQuestionnaireService.java

---
+++ service/EmpQuestionnaireService.java
@@ -0,0 +1,20 @@
+package com.miniProject.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.miniProject.models.EmpQuestionnaire;
+import com.miniProject.repository.EmpQuestionnaireRepo;
+
+@Service
+public class EmpQuestionnaireService {
+	
+	@Autowired 
+	EmpQuestionnaireRepo repository;
+	
+	public void getData(int questId, String empId, int status) {
+		EmpQuestionnaire emp_ques= new EmpQuestionnaire(questId,empId,status);
+		repository.save(emp_ques);
+	}
+
+}
